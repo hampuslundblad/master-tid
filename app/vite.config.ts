fe3a9baf90ce5f7ae859200bf9e66c42c7c8f670
hampuslundblad <31845093+hampuslundblad@users.noreply.github.com>
2025-03-26 21:08:12 +0100

    add base to vite config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "master-tid",
   plugins: [react(), tailwindcss()],
   build: {
     outDir: "./dist",
